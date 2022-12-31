c6635612a8c4af8e7f4d3edbf61553d43aadf39d
Robert Yan <mstr.raphael@gmail.com>
2022-12-31 16:33:01 +0800

    feat: create a blog module and add IBC

---
+++ ts-client/planet.blog/types.ts
@@ -0,0 +1,11 @@
+import { BlogPacketData } from "./types/planet/blog/packet"
+import { NoData } from "./types/planet/blog/packet"
+import { Params } from "./types/planet/blog/params"
+
+
+export {     
+    BlogPacketData,
+    NoData,
+    Params,
+    
+ }
