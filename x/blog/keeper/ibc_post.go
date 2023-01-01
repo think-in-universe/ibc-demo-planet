7d3e624128390b640409c8952af195ead1ac68da
Robert Yan <mstr.raphael@gmail.com>
2023-01-01 10:01:46 +0800

    feat: update keeper

--- x/blog/keeper/ibc_post.go
+++ x/blog/keeper/ibc_post.go
@@ -73,7 +73,16 @@ func (k Keeper) OnRecvIbcPostPacket(ctx sdk.Context, packet channeltypes.Packet,
 		return packetAck, err
 	}
 
-	// TODO: packet reception logic
+	id := k.AppendPost(
+        ctx,
+        types.Post{
+            Creator: packet.SourcePort + "-" + packet.SourceChannel + "-" + data.Creator,
+            Title:   data.Title,
+            Content: data.Content,
+        },
+    )
+
+    packetAck.PostID = strconv.FormatUint(id, 10)
 
 	return packetAck, nil
 }
@@ -97,7 +106,15 @@ func (k Keeper) OnAcknowledgementIbcPostPacket(ctx sdk.Context, packet channelty
 			return errors.New("cannot unmarshal acknowledgment")
 		}
 
-		// TODO: successful acknowledgement logic
+		k.AppendSentPost(
+            ctx,
+            types.SentPost{
+                Creator: data.Creator,
+                PostID:  packetAck.PostID,
+                Title:   data.Title,
+                Chain:   packet.DestinationPort + "-" + packet.DestinationChannel,
+            },
+        )
 
 		return nil
 	default:
@@ -109,7 +126,14 @@ func (k Keeper) OnAcknowledgementIbcPostPacket(ctx sdk.Context, packet channelty
 // OnTimeoutIbcPostPacket responds to the case where a packet has not been transmitted because of a timeout
 func (k Keeper) OnTimeoutIbcPostPacket(ctx sdk.Context, packet channeltypes.Packet, data types.IbcPostPacketData) error {
 
-	// TODO: packet timeout logic
+	k.AppendTimedoutPost(
+        ctx,
+        types.TimedoutPost{
+            Creator: data.Creator,
+            Title:   data.Title,
+            Chain:   packet.DestinationPort + "-" + packet.DestinationChannel,
+        },
+    )
 
 	return nil
 }
